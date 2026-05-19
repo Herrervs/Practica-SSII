7c4270696b67e7e1769ef15bd1c86c6b7d102a88
Herrervs <jherreraballesteros05@gmail.com>
2026-05-19 20:20:34 +0200

    primera implementacion SentimentAnalyzer

--- src/practica/nlp/SentimentAnalyzer.java
+++ src/practica/nlp/SentimentAnalyzer.java
@@ -1,5 +1,21 @@
 package practica.nlp;
 
+import java.util.Set;
+
 public class SentimentAnalyzer {
-    
+    private static final Set<String> PALABRAS_NEGATIVAS = Set.of("alarma", "crisis", "catástrofe", "escándalo",
+            "fraude", "peligro", "terrible", "desastre", "conspiración", "urgente");
+    private static final Set<String> PATRONES_DE_CLICBAIT = Set.of("no te lo creerás", "te sorprenderá",
+            "todo el mundo habla", "lo que los medios ocultan", "la verdad que no quieren");
+
+    public double analyzeSentiment(String text) {
+        String textoMinusculas = text.toLowerCase();
+        long palNeg = PALABRAS_NEGATIVAS.stream().filter(textoMinusculas::contains).count();
+        long patClick = PATRONES_DE_CLICBAIT.stream().filter(textoMinusculas::contains).count();
+        // Score 0.0 (muy sospechoso) y 1.0 (neutro)
+        // palNeg penaliza el score por cada palabra negativa encontrada (0.15 permite que con 1-2 palabras el score no se penalice tanto. hacen falta unas 7 para que baje a 0)
+        // patClick penaliza el score por cada patrón de clickbait encontrado (0.3 permite que con 2 patrones de clickbait el score sea 0.4. hacen falta unas 4 para que baje a 0)
+        // math.max evita que el resultado sea negativo
+        return Math.max(0.0, 1.0 - (palNeg * 0.15) - (patClick * 0.3));
+    }
 }
