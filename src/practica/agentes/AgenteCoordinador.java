b42069dc1ec843716baaaa74e90a78f290f33991
Eva <evapomboal@gmail.com>
2026-05-23 17:23:00 +0200

    Creación AgenteCoordinador y AgenteVisualizacion

--- src/practica/agentes/AgenteCoordinador.java
+++ src/practica/agentes/AgenteCoordinador.java
@@ -1,5 +1,23 @@
 package practica.agentes;
 
-public class AgenteCoordinador {
-    
+import jade.core.Agent;
+import practica.behaviour.AgregarResultadosBehaviour;
+import practica.behaviour.DelegarAnalisisBehaviour;
+
+public class AgenteCoordinador extends Agent {
+
+    protected void setup() {
+        System.out.println("[COORDINADOR] ¡Hola! El agente" + getLocalName() + " ha arrancado.");
+
+        // Escucha noticias nuevas y las reparte a los 3 expertos
+        addBehaviour(new DelegarAnalisisBehaviour(this));
+
+        // Escucha las notas de los expertos, las suma y crea el informe final
+        addBehaviour(new AgregarResultadosBehaviour(this));
+    }
+
+    protected void takeDown() {
+        System.out.println("[COORDINADOR] El agente " + getLocalName() + " se está apagando.");
+    }
+
 }
--- src/practica/agentes/AgenteVisualizacion.java
+++ src/practica/agentes/AgenteVisualizacion.java
@@ -1,5 +1,19 @@
 package practica.agentes;
 
-public class AgenteVisualizacion {
-    
+import jade.core.Agent;
+import practica.behaviour.ActualizarUIBehaviour;
+
+public class AgenteVisualizacion extends Agent {
+
+    protected void setup() {
+        System.out.println("[VISUALIZACION] ¡Hola! El agente" + getLocalName() + " ha arrancado.");
+
+        // Lanzamos la ventana grafica y nos quedamos escuchando informes
+        addBehaviour(new ActualizarUIBehaviour(this));
+    }
+
+    protected void takeDown() {
+        System.out.println("[VISUALIZACION] El agente " + getLocalName() + " se está apagando.");
+    }
+
 }
